35b163dde8821c6768f103c8cb6a63ff06661a7c
ant-droid <ant.dobr24@mail.ru>
2024-11-03 22:04:48 +0500

    v.1.2: add text recognition

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,25 +1,48 @@
-from aiogram import types, Router
+from string import punctuation
+
+from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command
 
 user_private_router = Router()
 
+@user_private_router.message((F.text.lower() == 'start') | (F.text.lower() == 'старт'))
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
     await message.answer('Привет, я виртуальный помощник')
 
+@user_private_router.message((F.text.lower() == 'menu') | (F.text.lower() == 'меню'))
 @user_private_router.message(Command('menu'))
 async def menu_cmd(message: types.Message):
     await message.answer("Вот меню:")
 
+@user_private_router.message((F.text.lower() == 'help') | (F.text.lower() == 'помощь'))
 @user_private_router.message(Command('help'))
 async def menu_cmd(message: types.Message):
     await message.answer("Вызов помощи!")
 
+@user_private_router.message((F.text.lower() == 'lmao') | (F.text.lower() == 'лмао'))
 @user_private_router.message(Command('lmao'))
 async def popopo_cmd(message: types.Message):
-    await message.answer("OOOOO LMAO")
+    await message.answer("AYY LMAO")
 
-edu = ['эдукон','едукон','educon','эдьюсон']
+
+def clean_text(text: str):
+    return text.translate(str.maketrans('','',punctuation))
+
+edu = {'эдукон','едукон','educon','эдьюсон'}
+
+@user_private_router.message()
+async def magic_cmd(message: types.Message):
+    if any(keyword in clean_text(message.text.lower().replace(" ","")) for keyword in edu):
+        await message.answer("Это магический фильтр")
+
+
+'''
+@user_private_router.message(lambda c: c.data in edu)
+async def magic_cmd(message: types.Message):
+    await message.answer("Это магический фильтр")
+'''
+"""
 @user_private_router.message()
 async def educon_cmd(message: types.Message):
     text = message.text
@@ -27,3 +50,9 @@ async def educon_cmd(message: types.Message):
         if i in text:
             await message.answer("educon answer")
             break
+"""
+'''
+@user_private_router.message(F.text.lower().contains("educon"))
+async def magic_cmd(message: types.Message):
+    await message.answer("Это магический фильтр")
+'''
